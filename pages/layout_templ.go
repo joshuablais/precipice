7eef65a6a721f5315626374610248fa93195814d
Joshua Blais <josh@joshblais.com>
2026-02-19 09:48:31 -0700

    container

--- pages/layout_templ.go
+++ pages/layout_templ.go
@@ -50,7 +50,7 @@ func Layout(title string, cfg *config.Config) templ.Component {
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, " - Precipice</title><link rel=\"stylesheet\" href=\"/static/main.css\"><!-- Use local Datastar --><script type=\"module\" src=\"/static/datastar.js\"></script></head><body class=\"\"><div>")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 2, " - Precipice</title><link rel=\"stylesheet\" href=\"/static/main.css\"><!-- Use local Datastar --><script type=\"module\" src=\"/static/datastar.js\"></script></head><body class=\"container\"><div>")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
