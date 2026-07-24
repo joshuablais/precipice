f01728e5602ce16bfc8f0d97c2b1f452802bb855
Joshua Blais <josh@joshblais.com>
2026-07-24 13:27:24 -0600

    guix channel/manifest

---
+++ channels.scm
@@ -0,0 +1,11 @@
+(cons*
+(channel
+ (name 'renaissance)
+ (url "https://codeberg.org/joshuablais/renaissance")
+ (branch "main")
+ (introduction
+  (make-channel-introduction
+   "e010cef398ba706197d8245b6e2a53cdcdb7e43a"
+   (openpgp-fingerprint
+    "F7A0 568B 4ECB CA9B 852B  3B03 3BCE FE7A E837 AFD7"))))
+ %default-channels)
---
+++ manifest.scm
@@ -0,0 +1,18 @@
+(use-modules (guix packages)
+             (guix profiles)
+             (gnu packages golang)
+             (gnu packages golang-build)
+             (gnu packages golang-check)
+             (gnu packages golang-xyz)
+             (gnu packages web)
+             (gnu packages rust-apps)
+             (renaissance packages go)
+             (renaissance packages air)
+             )
+
+(specifications->manifest '("go"
+                            "delve"
+                            "go-tools"
+                            "templ"
+                            "air"
+                            "gopls"))
